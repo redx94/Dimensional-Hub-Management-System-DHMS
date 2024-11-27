6e067070a053520fec278655fe47239d8f47a9dc
Reco <quantascriptor@gmail.com>
2024-11-27 03:00:55 -0500

    Update database schema for detailed functionality

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,18 @@
+CREATE DATABASE dimensional_hub_db
+
+\c diversional_hub_db;
+
 CREATE TABLE ubts (
-    id SERIAL DECIMAL PRIMARY,
+    id SERIAL PRIMARY INTEGER,
     name VARCHAR(20),
     status VARCHAR(20),
     stability INT)
-D;
+);
+
+CREATE TABLE COMMANDS(
+    id SERIAL DECIMAL PRIMARY,
+    hub_id INT REFERENCES hubs(id),
+    command TYXT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+part_graps_connect (=:base_shelld)
+normalizes *==new_message
