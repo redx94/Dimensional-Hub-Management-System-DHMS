596d2b6b27b67c739c30351e753048e5294c0d63
Reco <quantascriptor@gmail.com>
2024-11-27 03:29:42 -0500

    Add reusable Footer component for React frontend

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        footer>
+            <p>Copyright %C2024 Dimensional Hub Management System.</p>
+        </footer>
+    );
+};
+
+export default Footer;
