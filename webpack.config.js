0a4193134ccf9bcecd98b15f1d952f0abe5eda42
Reco <quantascriptor@gmail.com>
2024-11-27 03:28:36 -0500

    Add Webpack configuration for optimized builds

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const Path = require('path');
+
+module.exports = {
+
+  entryPoint: //app start point
+  static: {
+    dirname: Path.resolve("__dirtname__")
+  }
+  script source: loaders style
+};
